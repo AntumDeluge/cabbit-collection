90ef6f8613e855c869c0394b82d9d30ead912898
Jordan Irwin <antumdeluge@gmail.com>
2018-04-24 14:38:23 -0700

    Update release.sh script

--- release.sh
+++ release.sh
@@ -15,13 +15,14 @@ Description:
 Licensing:
   - OpenGameArt.org Attribution (OGA BY) version 3.0 or later (see: LICENSE-OGA-BY-3.0.txt)
   - Creative Commons Attribution (CC BY) version 3.0 or later (see: LICENSE-CC-BY-3.0.txt)
-Attribution:
-  Svetlana Kushnariova (Cabbit)
-  diamonddmgirl
-  Jordan Irwin (AntumDeluge)
+Copyright/Attribution:
+  Created by Svetlana Kushnariova (Cabbit) <lana-chan@yandex.ru>, diamonddmgirl, & Jordan Irwin (AntumDeluge)
 Links:
   - OpenGameArt.org page: https://opengameart.org/node/79804
-  - For links to Cabbit's & diamonddmgirl's submissions, see "sources.md" file."
+  - For links to Cabbit's & diamonddmgirl's submissions, see "sources.md" file.
+Notes:
+  - IMPORTANT: As part of Cabbit's attribution requirements, please credit her as \"Svetlana Kushnariova\"
+    & include her email address: lana-chan@yandex.ru"
 
 SOURCES="### Cabbit Collection Sources:
 
